558a5e7a84c7778a9c473ec828f48c13474453a7
Loïc <loic.laurent@outlook.com>
2022-10-22 10:50:46 +0200

    Edition de données capteurs

---
+++ src/Form/SensorDataType.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\SensorData;
+use App\Entity\Sensor;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+
+class SensorDataType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('value')
+            ->add('date')
+            ->add('sensor', EntityType::class, [
+                'class' => Sensor::class,
+                'choice_label'  => 'name'
+            ] )
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => SensorData::class,
+        ]);
+    }
+}
