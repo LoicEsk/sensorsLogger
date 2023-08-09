47773255126b644dc90aca34f03049be02776ff3
Loïc <loic.laurent@outlook.com>
2023-08-09 08:41:51 +0200

    update bootstrap

---
+++ deploy_docker.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+echo "Deployement Symfony"
+docker compose run --rm sf composer install
+
+# Migrations Doctrines
+docker compose exec sf php bin/console doctrine:migrations:list
+docker compose exec sf php bin/console doctrine:migrations:migrate
+
+echo "Install des packages node"
+docker compose run --rm encore npm ci
+
+echo "Compilation des Assets"
+docker compose stop encore
+docker compose up -d
